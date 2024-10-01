50836d8d4b46a6d87c24c86ddbe1f79cf94cec97
yunumu <1416900346@qq.com>
2024-10-01 23:50:07 +0800

    集成数据库：添加 TypeORM、MySQL 依赖及调整 RxJS 版本
    - 引入 @nestjs/typeorm 包，用于整合 TypeORM 到 NestJS 项目
    - 添加 mysql2 作为 MySQL 数据库驱动
    - 安装 typeorm，提供 ORM 功能
    - 更新 RxJS 版本，确保与新引入的包兼容

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,6 +4,7 @@ import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -16,6 +17,22 @@ import { HealthModule } from './health/health.module';
         PORT: Joi.number().default(3000),
         DATABASE_URL: Joi.string().required(),
       }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'yun',
+      database: 'a',
+      synchronize: true,
+      logging: true,
+      entities: [],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
     }),
     HealthModule,
   ],
