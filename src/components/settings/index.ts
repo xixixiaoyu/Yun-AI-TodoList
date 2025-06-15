7e674d63336de4c6daeaf9981ad99634312fcd37
yunmu <1416900346@qq.com>
2025-06-15 16:44:30 +0800

    refactor(settings): 重构提示词管理组件并优化i18n导入路径
    将原有的PromptTemplateSelector和CustomPromptManager组件替换为增强版本EnhancedCustomPromptManager和EnhancedPromptTemplateSelector
    优化i18n模块的导入路径，从直接导入改为从index文件导入
    删除不再使用的web-speech-api类型定义和android环境配置脚本

--- src/components/settings/index.ts
+++ src/components/settings/index.ts
@@ -3,7 +3,7 @@
  */
 
 export { default as ApiKeySection } from './ApiKeySection.vue'
-export { default as SystemPromptSection } from './SystemPromptSection.vue'
-export { default as CustomPromptManager } from './CustomPromptManager.vue'
+export { default as EnhancedCustomPromptManager } from './EnhancedCustomPromptManager.vue'
+export { default as EnhancedPromptTemplateSelector } from './EnhancedPromptTemplateSelector.vue'
 export { default as SettingsToast } from './SettingsToast.vue'
-export { default as PromptTemplateSelector } from './PromptTemplateSelector.vue'
+export { default as SystemPromptSection } from './SystemPromptSection.vue'
